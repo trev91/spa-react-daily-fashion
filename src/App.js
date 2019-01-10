da339cfd338de4fc3bafbf6823e0aa9b54e19ec1
Trevor Hardy <trevor@vertitrev.com>
2019-01-10 16:50:37 -0700

    added basics for future chat feature

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import About from "./components/about";
 import NavMenu from "./components/navMenu";
 import Bag from "./components/bag";
 import BagIcon from "./assets/animations/iconBag";
+import { Widget, addResponseMessage, addLinkSnippet } from "react-chat-widget";
+
+import "react-chat-widget/lib/styles.css";
 import "./styling/app.scss";
 
 class App extends Component {
@@ -14,22 +17,48 @@ class App extends Component {
       bagOpen: false
     };
   }
+
+  // we'll use this eventually to send messages to the API
+  sendUsersMessage = message => {
+    console.log("We need to send this message to the backend!", message);
+    setTimeout(() => {
+      addResponseMessage("We're not available right now.");
+      addLinkSnippet({
+        title: "Reach out to our support team by sending us a quick email!",
+        link: "mailto:support@dailyfashion.com"
+      });
+    }, 2500);
+  };
+
   render() {
     return (
       <Router>
         <div id="App">
           <NavMenu />
           <main id="page">
-            <Bag visible={this.state.bagOpen} items={JSON.parse(localStorage.getItem('bag'))} />
+            <Widget
+              handleNewUserMessage={this.sendUsersMessage}
+              title="Let's Chat!"
+              subtitle="We're available during regular business hours"
+            />
+
+            <Bag
+              visible={this.state.bagOpen}
+              items={JSON.parse(localStorage.getItem("bag"))}
+            />
             <div className="app-header large-12 small-12 text-center">
               <div className="row">
                 <div className="branding large-12 columns">
                   <h1 className="primary-color">Daily Fashion</h1>
-                  <h6>One specially curated piece, for one day only.</h6>
+                  <h6>One of a kind clothing, one day only.</h6>
                 </div>
                 <div
                   className="bag-button"
-                  onClick={() => this.setState({ bagOpen: true })}
+                  onClick={() =>
+                    this.setState({
+                      bagOpen: true
+                    })
+                  }
                 >
                   <BagIcon />
                 </div>
